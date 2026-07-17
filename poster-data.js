28b91fb31c59ae71118b4c23ecfbb2378564a251
Quranic Huffaz <notawanhasbullah@gmail.com>
2026-07-17 23:11:07 +0800

    Create poster-data.js

---
+++ poster-data.js
@@ -0,0 +1,28 @@
+const SITE_OWNER = "Wan Hasbullah";
+const SITE_YEAR = 2026;
+
+const posterData = {
+  subjects: [
+    {
+      id: "muqaddimah",
+      code: "MQ",
+      name: "Muqaddimah",
+      description: "Peta minda asas & pengenalan ilmu.",
+      icon: "book-open-text",
+      titles: [
+        {
+          id: "doa-keluar-tandas",
+          name: "Doa Keluar Tandas",
+          description: "Poster peta minda berkaitan doa keluar tandas.",
+          posters: [
+            {
+              file: "poster1.jpg",
+              title: "Poster 1",
+              tags: ["doa", "tandas", "muqaddimah"]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
